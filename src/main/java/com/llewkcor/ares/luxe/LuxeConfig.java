572c5a0190d2a16df6143e76079e52f0e0306133
j-rockwell <jrobertrockwell@gmail.com>
2020-04-09 20:43:18 -0700

    Added server ping MOTD and server join MOTD support

--- src/main/java/com/llewkcor/ares/luxe/LuxeConfig.java
+++ src/main/java/com/llewkcor/ares/luxe/LuxeConfig.java
@@ -1,8 +1,12 @@
 package com.llewkcor.ares.luxe;
 
+import com.google.common.collect.Lists;
 import com.llewkcor.ares.commons.util.general.Configs;
 import lombok.Getter;
+import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.util.List;
 
 public final class LuxeConfig {
     @Getter public final Luxe plugin;
@@ -10,6 +14,8 @@ public final class LuxeConfig {
     @Getter public int shoutInterval;
     @Getter public int premiumSlotAccessStart;
     @Getter public String storeLink;
+    @Getter public List<String> joinMotd;
+    @Getter public List<String> pingMotd;
 
     public LuxeConfig(Luxe plugin) {
         this.plugin = plugin;
@@ -21,5 +27,16 @@ public final class LuxeConfig {
         this.shoutInterval = config.getInt("premium_features.shout_cooldown");
         this.premiumSlotAccessStart = config.getInt("premium_features.premium_slot_start");
         this.storeLink = config.getString("premium_features.premium_store_link");
+
+        this.joinMotd = Lists.newArrayList();
+        this.pingMotd = Lists.newArrayList();
+
+        for (String line : config.getStringList("motd.server_list")) {
+            pingMotd.add(ChatColor.translateAlternateColorCodes('&', line));
+        }
+
+        for (String line : config.getStringList("motd.join")) {
+            joinMotd.add(ChatColor.translateAlternateColorCodes('&', line));
+        }
     }
 }
--- src/main/java/com/llewkcor/ares/luxe/listener/LuxeListener.java
+++ src/main/java/com/llewkcor/ares/luxe/listener/LuxeListener.java
@@ -1,6 +1,7 @@
 package com.llewkcor.ares.luxe.listener;
 
 import com.google.common.base.Joiner;
+import com.llewkcor.ares.commons.util.bukkit.Scheduler;
 import com.llewkcor.ares.luxe.Luxe;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -9,7 +10,9 @@ import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.server.ServerListPingEvent;
 
 @AllArgsConstructor
 public final class LuxeListener implements Listener {
@@ -26,5 +29,16 @@ public final class LuxeListener implements Listener {
                     ChatColor.RED + "Purchase a rank to bypass this restriction",
                     ChatColor.AQUA + plugin.getConfiguration().getStoreLink()));
         }
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        final Player player = event.getPlayer();
+        new Scheduler(plugin).sync(() -> plugin.getConfiguration().getJoinMotd().forEach(player::sendMessage)).delay(2L).run();
+    }
+
+    @EventHandler
+    public void onServerPing(ServerListPingEvent event) {
+        event.setMotd(Joiner.on("\n").join(plugin.getConfiguration().getPingMotd()));
     }
 }
